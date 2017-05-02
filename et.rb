353896810a14c462070d4bda5deba0ceb4054476
Jason Gauci <jjg@fb.com>
2017-05-01 20:31:24 -0700

    et v3.0.0

--- et.rb
+++ et.rb
@@ -1,9 +1,9 @@
 class Et < Formula
   desc "Remote terminal with IP roaming"
   homepage "https://mistertea.github.io/EternalTCP/"
-  url "https://github.com/MisterTea/EternalTCP/archive/et-v2.1.0.tar.gz"
-  version "2.1.0"
-  sha256 "dfe23b80f3b60cf15af54d5b0855e2dec73f0afeee31b5bc6dcb02f03fc5c8eb"
+  url "https://github.com/MisterTea/EternalTCP/archive/et-v3.0.0.tar.gz"
+  version "3.0.0"
+  sha256 "7053d8f1be9c9c9e5467bbc250dc4fba08843f38bd563c6cf4bc474b135e1269"
 
   depends_on "cmake" => :build
 
@@ -16,6 +16,48 @@ class Et < Formula
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
+    etc.install 'launcher/et' => 'et.cfg' unless File.exists? etc+'et.cfg'
+  end
+
+  plist_options :startup => true
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{opt_bin}/etserver</string>
+        <string>--cfgfile</string>
+        <string>#{etc}/et.cfg</string>
+        <string>--daemon</string>
+      </array>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>KeepAlive</key>
+      <false/>
+      <key>WorkingDirectory</key>
+      <string>#{HOMEBREW_PREFIX}</string>
+      <key>StandardErrorPath</key>
+      <string>/tmp/et_err</string>
+      <key>StandardOutPath</key>
+      <string>/tmp/et_err</string>
+      <key>HardResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+      <key>SoftResourceLimits</key>
+      <dict>
+        <key>NumberOfFiles</key>
+        <integer>4096</integer>
+      </dict>
+    </dict>
+    </plist>
+    EOS
   end
 
   test do
