1681c5b38b5f46118d0c3e3369515ca1070aadde
Jason Gauci <jjg@fb.com>
2017-06-29 17:22:46 -0700

    v3.1.0

--- et.rb
+++ et.rb
@@ -1,9 +1,9 @@
 class Et < Formula
   desc "Remote terminal with IP roaming"
   homepage "https://mistertea.github.io/EternalTCP/"
-  url "https://github.com/MisterTea/EternalTCP/archive/et-v3.0.6.tar.gz"
-  version "3.0.6"
-  sha256 "34860a8f75b3d1ed406a3d9eb73c1cf85435f639c0de4ef77d4b78e4b7812efc"
+  url "https://github.com/MisterTea/EternalTCP/archive/et-v3.1.0.tar.gz"
+  version "3.1.0"
+  sha256 "4194132b3c1f0a965ae8dc3651b1d1d4e197b8ffd193ffc05ab541bdf0145cd8"
 
   depends_on "cmake" => :build
 
