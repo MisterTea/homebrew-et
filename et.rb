b12e3be897dc1334fd92c5bc4883ededf62f359d
Jason Gauci <jgmath2000@gmail.com>
2021-05-28 09:58:23 -0500

    v6.1.8

--- et.rb
+++ et.rb
@@ -1,10 +1,10 @@
 class Et < Formula
   desc "Remote terminal with IP roaming"
   homepage "https://mistertea.github.io/EternalTerminal/"
-  url "https://github.com/MisterTea/EternalTerminal/archive/et-v6.1.7.tar.gz"
+  url "https://github.com/MisterTea/EternalTerminal/archive/et-v6.1.8.tar.gz"
   head "https://github.com/MisterTea/EternalTerminal.git"
-  version "6.1.7"
-  sha256 "ef70076a40a4b14153a3cfdf4539325207330379200a321ceff8d7ac84d30f8d"
+  version "6.1.8"
+  sha256 "c82c62722d5e4db5b4596bcaafd4a4cf8c236968cb3782f791f5c38a62b10fb9"
   revision 1
 
   depends_on "cmake" => :build
